f37e567429b8a69b7272ba40a29de754ee16b440
Jules DUBOIS <152026944+juscodingquantum@users.noreply.github.com>
2024-05-19 12:55:07 +0200

    Add files via upload

---
+++ bot_force_ready.py
@@ -0,0 +1,50 @@
+import time
+import pyautogui
+# dimensions du macbook M1 en réference : 2 560 x 1 600 pixels
+def main():
+    pyautogui.click(1220, 800)
+
+    pyautogui.click(1220, 700)
+
+    pyautogui.click(1220, 650)
+
+    pyautogui.click(1220, 600)
+
+    pyautogui.click(1220, 550)
+
+    pyautogui.click(1220, 500)
+
+    pyautogui.click(1220, 450)
+
+    pyautogui.click(1220, 400)
+
+    pyautogui.click(1220, 350)
+
+    pyautogui.click(1220, 300)
+
+    pyautogui.click(1220, 250)
+
+    pyautogui.click(1220, 200)
+
+    pyautogui.click(1220, 150)
+
+    pyautogui.click(1220, 100)
+
+    pyautogui.click(1220, 50)
+
+    pyautogui.click(1220, 0)
+
+    pyautogui.click(1220, 50)
+
+    pyautogui.click(1220, 100)
+
+    pyautogui.click(1220, 150)
+
+    pyautogui.click(1220, 200)
+
+    pyautogui.click(500, 1450)
+
+
+
+if __name__ == "__main__":
+    main()
