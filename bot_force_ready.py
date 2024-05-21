47e6303aaf4714a104525af196faebd51d77e3d4
Jules DUBOIS <152026944+juscodingquantum@users.noreply.github.com>
2024-05-21 19:07:49 +0200

    Update bot_force_ready.py
    Changement bouton + position de bouton permanente

--- bot_force_ready.py
+++ bot_force_ready.py
@@ -1,50 +1,7 @@
-import time
 import pyautogui
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-# dimensions du macbook M1 en réference : 2 560 x 1 600 pixels
-def main():
-    pyautogui.click(1542, 257)
-
-    pyautogui.click(1542, 339)
-
-    pyautogui.click(1542, 410)
-
-    pyautogui.click(1542, 482)
-
-    pyautogui.click(1542, 558)
-
-    pyautogui.click(1542, 640)
-
-    pyautogui.click(1542, 698)
-
-    pyautogui.click(1542, 778)
-
-    pyautogui.click(1542,855 )
-
-    pyautogui.click(1542, 940)
-
-    pyautogui.click(1420, 261)
-
-    pyautogui.click(1420, 341)
-
-    pyautogui.click(1420, 413)
-
-    pyautogui.click(1420, 486)
-
-    pyautogui.click(1420, 559)
-
-    pyautogui.click(1420, 635)
-
-    pyautogui.click(1420, 710)
-
-    pyautogui.click(1420, 783)
-
-    pyautogui.click(1420, 854)
-
-    pyautogui.click(1420, 935)
-
-    pyautogui.click(184,105)
+from PyQt5.QtCore import Qt
 
 class App(QWidget):
 
@@ -53,22 +10,41 @@ class App(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("Bouton cliquable")
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)  # Garde la fenêtre toujours en avant-plan et sans bordure
+        self.setGeometry(20, 490, 60, 60)  # Positionne la fenêtre à 20x490 avec une taille de 60x60
 
         button = QPushButton("Cliquez-moi !", self)
         button.clicked.connect(self.button_click)
-
+        button.setStyleSheet("border: 0px;")
+        button.setFixedSize(60, 60)
+        button.move(0, 0)  # Positionne le bouton à l'intérieur de la fenêtre
         self.show()
 
     def button_click(self):
         # Fonction à exécuter lorsque le bouton est cliqué
-        print("Bouton cliqué !")
+        pyautogui.click(1542, 257)
+        pyautogui.click(1542, 339)
+        pyautogui.click(1542, 410)
+        pyautogui.click(1542, 482)
+        pyautogui.click(1542, 558)
+        pyautogui.click(1542, 640)
+        pyautogui.click(1542, 698)
+        pyautogui.click(1542, 778)
+        pyautogui.click(1542, 855)
+        pyautogui.click(1542, 940)
+        pyautogui.click(1420, 261)
+        pyautogui.click(1420, 341)
+        pyautogui.click(1420, 413)
+        pyautogui.click(1420, 486)
+        pyautogui.click(1420, 559)
+        pyautogui.click(1420, 635)
+        pyautogui.click(1420, 710)
+        pyautogui.click(1420, 783)
+        pyautogui.click(1420, 854)
+        pyautogui.click(1420, 935)
+        pyautogui.click(184, 105)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
-
-
-if __name__ == "__main__":
-    main()
